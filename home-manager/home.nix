4b37acc667a732de6f30577600712b60c108f2f7
Lewis Flude <lewis@lewisflude.com>
2025-02-21 22:17:15 +0000

    Add home.nix

---
+++ home-manager/home.nix
@@ -0,0 +1,20 @@
+{ ...
+}: {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "lewis";
+    homeDirectory = "/home/lewis";
+  };
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "24.05";
+}
