5fb592ddd63ea9eb49526f2fa42596c2cfbec4c3
Lewis Flude <lewis@lewisflude.com>
2025-02-21 22:08:20 +0000

    Add initial setup to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,3 +1,28 @@
 {
-  outputs = { self }: { };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+    let inherit (self) outputs; in {
+
+      nixosConfigurations = {
+        jupiter = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs outputs;
+          };
+          modules = [ ./nixos/configuration.nix ];
+        };
+      };
+      homeConfigurations = {
+        "lewis@jupiter" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ ./home-manager/home.nix ];
+        };
+      };
+    };
 }
