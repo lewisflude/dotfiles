d9d0f6289cf190162792b6468afce101e7f41cf6
Lewis Flude <lewis@lewisflude.com>
2025-02-23 14:23:11 +0000

    Change greetd config

--- nixos/modules/desktop-environment.nix
+++ nixos/modules/desktop-environment.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     wofi
     yazi
@@ -15,10 +15,26 @@
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+
+    cursorTheme = {
+      name = "catppuccin-mocha-mauve-cursors";
+      package = pkgs.catppuccin-cursors.mochaMauve;
+    };
+
+
     settings = {
       commands = {
-        reboot = ["systemctl" "reboot"];
-        poweroff = ["systemctl" "poweroff"];
+        reboot = [ "systemctl" "reboot" ];
+        poweroff = [ "systemctl" "poweroff" ];
+      };
+    };
+  };
+
+  services.greetd = {
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland -q";
+        user = "lewis";
       };
     };
   };
