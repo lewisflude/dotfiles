431704c69a161650f57008969b5b1798dd2eee81
Lewis Flude <lewis@lewisflude.com>
2025-02-24 15:59:38 +0000

    Add virtualbox

--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -1,8 +1,17 @@
-{...}: {
+{ ... }: {
   virtualisation = {
     docker = {
       enable = true;
       storageDriver = "zfs";
     };
+
+    virtualbox = {
+      host.enable = true;
+      host.enableExtensionPack = true;
+      guest.enable = true;
+      guest.dragAndDrop = true;
+    };
   };
+
+  users.extraGroups.vboxusers.members = [ "lewis" ];
 }
