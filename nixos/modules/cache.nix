12771616041c8cd3719de9113629fc1c09b288d4
Lewis Flude <lewis@lewisflude.com>
2025-02-25 10:39:49 +0000

    Update cache

--- nixos/modules/cache.nix
+++ nixos/modules/cache.nix
@@ -1,6 +1,18 @@
-{...}: {
+{ ... }: {
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://ags.cachix.org"
+      "https://catppuccin.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3TvwYI="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKroh/pfGgUZbiGQwvZP9p59M9Fk="
+    ];
   };
 }
