6fad81af10b71d7dc66940bb3370f070ea0d5ca0
Lewis Flude <lewis@lewisflude.com>
2025-02-27 00:08:21 +0000

    Update key

--- home-manager/modules/version-control.nix
+++ home-manager/modules/version-control.nix
@@ -2,7 +2,7 @@
   programs.git = {
     enable = true;
     userEmail = "lewis@lewisflude.com";
-    signing.key = "0x4EE1F251042A46F1";
+    signing.key = "0x221F0B8B8FFF5BD3";
     extraConfig.commit.gpgsign = true;
   };
 }
